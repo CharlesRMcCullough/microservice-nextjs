ecea22f25503f683491d5cd29d1007ec7d164291
Charles McCullough <charles@charlesmccullough.com>
2024-08-18 13:58:12 -0500

    Search Service Load Working

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -2,27 +2,37 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
-public class AuctionsController(AuctionDbContext context, IMapper mapper) : ControllerBase
+[Route("api/auctions")]
+
+public class AuctionsController : ControllerBase
 {
-    private readonly AuctionDbContext _context = context;
-    private readonly IMapper _mapper = mapper;
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
     {
-        var auctions = await _context.Auctions
-            .Include(x => x.Item)
-            .OrderBy(x => x.Item.Make)
-            .ToListAsync();
-
-        return _mapper.Map<List<AuctionDto>>(auctions);
+        var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+        
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+        
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     [HttpGet("{id}")]
---
+++ src/SearchService/Search/AuctionSvcHttpClient.cs
@@ -0,0 +1,28 @@
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Search;
+
+public class AuctionSvcHttpClient
+{
+
+    private readonly IConfiguration _config;
+    private readonly HttpClient _httpClient;
+
+    public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+    {
+        _httpClient = httpClient;
+        _config = config;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(a => a.UpdatedAt))
+            .Project(p => p.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+       
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"]
+            + "/api/auctions?date=" + lastUpdated);
+    }
+}
